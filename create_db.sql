4710696de655c90c71e41d039602ccd4f9d0755f
agirg001 <girginazra@hotmail.com>
2023-12-13 13:10:16 +0000

    db

--- create_db.sql
+++ create_db.sql
@@ -7,7 +7,7 @@ USE ForumApp;
 # Create the app user and give it access to the database
 CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
 GRANT ALL PRIVILEGES ON ForumApp.* TO 'appuser'@'localhost';
-
+CREATE TABLE Topics (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
 
 
