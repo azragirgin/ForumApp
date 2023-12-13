1dd8c3c33a14f23dee79e98c84ac82144e97a641
agirg001 <girginazra@hotmail.com>
2023-12-13 13:19:09 +0000

    db

--- create_db.sql
+++ create_db.sql
@@ -1,20 +1,22 @@
 # Create database script for Forum App
 
 # Create the database
-CREATE DATABASE ForumApp;
+CREATE DATABASE IF NOT EXISTS ForumApp;
 USE ForumApp;
 
 # Create the app user and give it access to the database
 CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
 GRANT ALL PRIVILEGES ON ForumApp.* TO 'appuser'@'localhost';
-CREATE TABLE Topics (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+
+
+CREATE TABLE IF NOT EXISTS Topics (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
 
 
 
 
 # Create the tables
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -22,7 +24,7 @@ CREATE TABLE Users (
 );
 
 # Create the tables
-CREATE TABLE Forum (
+CREATE TABLE IF NOT EXISTS Forum (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -33,7 +35,7 @@ CREATE TABLE Forum (
 );
 
 # Create the tables
-CREATE TABLE Posts (
+CREATE TABLE IF NOT EXISTS Posts (
   id INT AUTO_INCREMENT PRIMARY KEY,
   content TEXT NOT NULL,
   user_id INT,
